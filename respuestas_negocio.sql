0bfc26784c5cbdc8d63d24bcbbec8b5278e4d853
AgusArdizzone <ardizzone.agustin@gmail.com>
2025-05-18 17:14:50 -0300

    comentarios en queries

--- respuestas_negocio.sql
+++ respuestas_negocio.sql
@@ -4,6 +4,12 @@
 -- 1. Listar los usuarios que cumplan años el día de hoy
 --    cuya cantidad de ventas realizadas en enero 2020 sea superior a 1500.
 
+/*
+Query to get data from the Customers that it's their birthday today
+and they have made 1500 sales or more in January 2020.
+We can see some info about the customer in the output like their ID,
+username, full name and email.
+*/
 SELECT c.ID as "Customer ID",
        c.USERNAME as "Customer Username",
        c.FIRST_NAME as "First Name",
@@ -11,12 +17,14 @@ SELECT c.ID as "Customer ID",
        c.EMAIL as "Email"
 FROM CUSTOMER c
 JOIN (
+--  Subquery to get the seller_id's with more than 1500 sales in January 2020.
     SELECT i.SELLER_ID
     FROM CUSTOMER_ORDER co
     JOIN ITEM i ON co.ITEM_ID = i.ID
     WHERE co.ORDER_DATE >= TO_DATE('2020-01-01', 'YYYY-MM-DD')
       AND co.ORDER_DATE < TO_DATE('2020-02-01', 'YYYY-MM-DD')
     GROUP BY i.SELLER_ID
+-- To prevent any duplicates, we use DISTINCT on the order ID.
     HAVING COUNT(DISTINCT CO.ID) > 1500
 ) s ON C.ID = s.SELLER_ID
 WHERE EXTRACT(MONTH FROM c.BIRTH_DATE) = EXTRACT(MONTH FROM SYSDATE)
@@ -29,6 +37,13 @@ WHERE EXTRACT(MONTH FROM c.BIRTH_DATE) = EXTRACT(MONTH FROM SYSDATE)
 --    cantidad de ventas realizadas, cantidad de productos vendidos
 --    y el monto total transaccionado.
 
+/*
+Query that list the top 5 sellers of the category "Celulares" for each month of 2020.
+We can see the month rank, the month and year, info about the seller
+and different sales metrics.
+For better readability of the information, 
+we ordered it by month and year (most recently first) and rank.
+*/
 SELECT 
     month_rank as "Month Rank",
     year_month as "Year Month",
@@ -38,6 +53,10 @@ SELECT
     total_items_sold as "Total Items Sold",
     total_amount as "Total Amount"
 FROM (
+-- Subqquery to get a rank of the sellers by month and year,
+-- alongside calculating the sales metrics. and filtering by category and year.
+-- We use the ROW_NUMBER function instead of RANK or DENSE_RANK since these
+-- functions could return more than 5 sellers in case of a tie.
 SELECT
     trunc(co.ORDER_DATE, 'MM') AS year_month,
     C.FIRST_NAME,
@@ -64,6 +83,7 @@ GROUP BY
     c.FIRST_NAME,
     c.LAST_NAME
 )
+-- We just want the first top 5 seellers of each month.
 where month_rank <= 5
 ORDER BY year_month desc, month_rank;
 
